abd8cdd1bc39a887951107f2434460b9d875481e
root <root@betable1.dib.io>
2014-01-19 21:19:45 -0500

    First commit

---
+++ modules/helloworld/manifests/init.pp
@@ -0,0 +1,20 @@
+class helloworld {
+  include nodejs
+
+#Add the python-software-properties package to install ppa
+  package { 'python-software-properties':
+    ensure => latest,
+  }
+
+#Add the repo to get a newer nodejs
+apt::ppa { 'ppa:richarvey/nodejs': }
+
+# Install nodejs and packages
+  package { 'express':
+    ensure   => present,
+    provider => 'npm',
+    require => Class['nodejs'],
+  }
+
+}
+
